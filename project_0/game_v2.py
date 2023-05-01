dc54459784a604560dbe4fc0b5a0c5301a56bca1
IvanRatelin <admirerlin@gmail.com>
2023-05-01 13:37:30 +0600

    Убрал лишнии строки

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -4,23 +4,13 @@
 
 import numpy as np 
 
-# def random_predict(number:int = 1) -> int:
-#     """ Рандомно угадываем число
 
-#     Args:
-#         number (int, optional): Загаданное число. Defaults to 1.
-
-#     Returns:
-#         int: Число попыток
-#     """
-#     count = 0
-    
-#     while True:
-#         count+=1
-#         predict_number = np.random.randint(1, 101) #предполагаемое число
-#         if number == predict_number:
-#             break # выходит из цикла если угадано2        
-#     return (count)
+""" Рандомно угадываем число
+    Args:
+       number (int, optional): Загаданное число. Defaults to 1.
+    Returns:
+       int: Число попыток
+    """
 
 def random_predict(number:int = 1) -> int:
     count, a, z = 0, 1, 101
@@ -37,27 +27,15 @@ def random_predict(number:int = 1) -> int:
      
     return (count)
 
-# def score_game(random_predict) -> int:
-#     """За какое количество попыпоток в среднем угадывает за 1000 подходов наш алгоритм
+    """За какое количество попыпоток в среднем угадывает за 1000 подходов наш алгоритм
 
-#     Args:
-#         random_predict (_type_): функция угадывания
+     Args:
+         random_predict (_type_): функция угадывания
 
-#     Returns:
-#         int: среднее количество угадываний
-#     """
-#     count_ls = []
-#     np.random.seed(1) # фиксируем сид для воспроизводимости
-#     random_array =  np.random.randint(1, 11, size=(10)) # загадали список чисел
-#     for number in random_array:
-#         count_ls.append(random_predict(number))
-#     score = int(np.mean(count_ls))
-#     print(f'Ваш алгоритм угадывает число в среднем за {score} попыток.')
-#     return(score)
-
-# score_game(random_predict)
-
-
+     Returns:
+         int: среднее количество угадываний
+     """
+    
 def score_game(random_predict) -> int:
     count_ls = []
     np.random.seed(1)
