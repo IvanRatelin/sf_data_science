427f515d90357b44e06a5bf45d536fd56036fcb2
IvanRatelin <admirerlin@gmail.com>
2023-05-02 10:02:54 +0600

    Update game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -4,6 +4,7 @@
 
 import numpy as np 
 
+
 def random_predict(number:int = 1) -> int:
     """ Рандомно угадываем число
     Args:
@@ -13,13 +14,13 @@ def random_predict(number:int = 1) -> int:
     """    
     count, a, z = 0, 1, 101
     while True:
-        count+=1
+        count += 1
         predict_number = np.random.randint(a, z) 
         if number == predict_number:
             break 
-        if (number < predict_number): 
+        if number < predict_number: 
             z = predict_number
-        if (number > predict_number):
+        if number > predict_number:
             a = predict_number
     return (count)
 
@@ -32,18 +33,17 @@ def score_game(random_predict) -> int:
 
      Returns:
          int: среднее количество угадываний
-     """
+    """
     count_ls = []
     np.random.seed(1)
     random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(random_predict(number))
     score = int(np.mean(count_ls))
-    print(f'Ваш алгоритм угадывает число в среднем за {score} попыток')
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     score_game(random_predict)
 
-# пометка для себя (УДАЛИТЬ) среда разработки дома 3.10.6
